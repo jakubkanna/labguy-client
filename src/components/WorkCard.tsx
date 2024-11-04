bfe519f28e475fbdeaed2b1190566b483c2711ca
j <hello.jakubkanna@gmail.com>
2024-11-04 14:44:18 +0100

    workcard fix

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -13,6 +13,9 @@ interface CardProps {
 export default function WorkCard({ work }: CardProps) {
   const { general, dimensions, year, media } = work;
   const { title, slug } = general;
+
+  if (!media) return;
+
   const image = isImage(media[0] as MediaRef) && (media[0] as MediaRef);
   const video = isVideo(media[0] as MediaRef) && (media[0] as MediaRef);
   return (
